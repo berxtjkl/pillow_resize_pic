087d1670a205647e0b98957eb365b6c16bf99718
JesseZhang <jessejunjing@163.com>
2020-01-16 12:56:17 +0800

    添加crop代码, 现在不仅可以resize, 还可以crop

--- resizePic.py
+++ resizePic.py
@@ -3,15 +3,26 @@ originPicName = 'akashi'
 img = Image.open('./'+originPicName+'.jpg')  # image extension *.png,*.jpg
 tempList = ["100,100", "200,200", "300,300", "400,400", "500,500", "600,600", "700,700",
             "800,800", "900,900", "1000,1000", "1100,1100", "1200,1200", "1300,1300", "1400,1400", "1500,1500"]
-tempList1 = ["200,100", "300,200", "400,300", "500,400", "600,500", "700,600", "800,700",
-            "900,800", "1000,900"]
-for i in tempList1:
-    temp = i.split(',')
-    print(temp[0])
-    new_width = temp[0]
-    print(temp[1])
-    new_height = temp[1]
-    img = img.resize((int(new_width), int(new_height)), Image.ANTIALIAS)
-    img.save('./sets/originPicName-'+new_width+"-"+new_height +
-             ".jpg", quality=95)  # format may what u want ,*.png,*jpg,*.gif
+tempList1 = "100"
+def resize(img):
+    for i in tempList:
+        temp = i.split(',')
+        print(temp[0])
+        new_width = temp[0]
+        print(temp[1])
+        new_height = temp[1]
+        img = img.resize((int(new_width), int(new_height)), Image.ANTIALIAS)
+        img.save('./sets/originPicName-resize-'+new_width+"-"+new_height +
+                ".jpg", quality=95)  # format may what u want ,*.png,*jpg,*.gif
+
+def crop(img):
+    img.show()
+    # 传入参数分别是左上的坐标和右下的坐标
+    img = img.crop((0,0,500,500))
+    
+    img.save('./sets/originPicName-crop.jpg')
+
+
+crop(img)
+
 img.close()
